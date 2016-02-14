3550692d02646d9a400f53fa171c7915238d1141
Marti Serrallonga <msb_bcn@hotmail.com>
2016-02-14 04:09:21 +0100

    add status list

--- FindIntersections/main.cpp
+++ FindIntersections/main.cpp
@@ -21,6 +21,25 @@ bool y_desc(vector2f* first, vector2f* second) {
 		return (first->x < second->x);
 }
 
+//S'haurà de modificar addPoint dins de eventList
+void addSegment(list <segment*> * status, vector2f* p) {
+	list<segment*>::iterator i = status->begin();
+	for (i; i != status->end(); i++) {
+		if (((*i)->getStart())->x >= p->x) {
+			break;
+		}
+	}
+	status->insert(i, p->father);
+}
+
+void printStatus(list <segment*> * status) {
+	cout << "Status: " << endl;
+	for (list<segment*>::iterator i = status->begin(); i != status->end(); i++) {
+		cout << (*i)->print() << endl;
+	}
+	cout << endl;
+}
+
 int main(int arg, char** argv) {
 
 	//Data entry
@@ -36,8 +55,12 @@ int main(int arg, char** argv) {
 	eventList.getList()->sort(y_desc);
 	eventList.addPoint(new vector2f(0.7, 3));
 	cout << eventList.print()<<endl;
-	
-	
+
+	list <segment*> status;
+	addSegment(&status, eventList.getList()->front());
+	addSegment(&status, eventList.getList()->back());
+	printStatus(&status);
+
 
 
 	system("pause");
